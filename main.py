5e4fedc4dc5e9f1272b72af12015bd0cdaf7d17f
Danix43 <daninitu39@gmail.com>
2021-03-24 12:03:54 +0200

    Added "Mapa pe M" feature

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from scraper import Scraper
 import discord
 import asyncio
-from datetime import datetime, time
 import pytz
-from dotenv import load_dotenv
 import os
+
+from scraper import Scraper
+from datetime import time, datetime
+from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -14,9 +15,10 @@ class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.tz = pytz.timezone("Europe/Bucharest")
         self.scraper = Scraper()
         self.factions_bg_task = self.loop.create_task(self.scrap_factions())
-        self.turfs_bg_task = self.loop.create_task(self.scrap_turfs())
+        self.turfs_bg_task = self.loop.create_task(self.run_at(time(22, 00, tzinfo=self.tz), self.scrap_turfs()))
 
     async def on_ready(self):
         print('Logged in as')
@@ -24,56 +26,83 @@ class MyClient(discord.Client):
         print(self.user.id)
         print('------')
 
-    async def scrap_factions(self):
-        async def create_message(data):
-            tz = pytz.timezone("Europe/Bucharest")
+    async def wait_until(self, time_to_run):
+        now = time.now()
+        await asyncio.sleep((time_to_run - now).total_seconds())
 
-            embed = discord.Embed(title="👉🏻 Statusul Aplicatiilor in Factiuni 👈🏻", 
-                                url="https://www.rpg2.b-zone.ro/factions/index",
-                                description=f"⏰ Ultima data actualizat la {datetime.now(tz)}⏰",
+    async def run_at(self, time_to_run, coro):
+        await self.wait_until(time_to_run)
+        return await coro
+
+
+    async def create_embed_info(self, title, link, data):
+        embed = discord.Embed(title=title, 
+                                url=link,
+                                description=f"⏰ Ultima data actualizat la {datetime.now(self.tz)}⏰",
                                 color=0x00ff62)
+        embed.set_author(name="Danix43", icon_url="https://cdn.discordapp.com/avatars/783680772014997546/92a2a4d507b520d27aa91121a8dece50.png")
+        embed.set_thumbnail(url="http://i.imgur.com/Z3UHdYS.png")
+        for k, v in data.items():
+            embed.add_field(name=k, value=v, inline=False)
+        return embed
+
+    async def on_message(self, message):
+        async def create_embed():
+            link_mapa = "http://bit.ly/3cilAMo"
+            link_discord = "https://discord.gg/yeZGaTfWnK"
+            
+            embed = discord.Embed(title="Mod Mapa Pe M cu Locatiile B-Zone",
+                                    description="Modul este inca in development, check for updates",
+                                    color=0xfc0303)
             embed.set_author(name="Danix43", icon_url="https://cdn.discordapp.com/avatars/783680772014997546/92a2a4d507b520d27aa91121a8dece50.png")
             embed.set_thumbnail(url="http://i.imgur.com/Z3UHdYS.png")
-            for k, v in data.items():
-                embed.add_field(name=k, value=v, inline=False)
+            embed.add_field(name="Join Us", value=link_discord, inline=False)
+            embed.add_field(name="Mod Link", value=link_mapa, inline=False)
             return embed
 
+        if message.author == self.user:
+            return
+
+        if message.content == "mapa pe m":
+            if message.channel.type == discord.ChannelType.private:
+                await message.channel.send(embed=await create_embed())
+            else:
+                return
+
+    async def scrap_factions(self):
         while True:
             await self.wait_until_ready()
             data = await self.scraper.check_factions()
             print("sending message")
-
             # dev channel
-            # await client.get_channel(813745203680378890).send(embed=await create_message(data))
+            # await client.get_channel(813745203680378890).send(
+            #     embed=await self.create_embed_info("👉🏻 Statusul Aplicatiilor in Factiuni 👈🏻", 
+            #                                 "https://www.rpg2.b-zone.ro/factions/index",
+            #                                     data))            
             # prod channel
-            await client.get_channel(813881450423386142).send(embed=await create_message(data))
-            print("done, waiting")
-            await asyncio.sleep(3600)
-            print("done waiting, resuming")
-    
-    async def scrap_turfs(self):
-        tz = pytz.timezone("Europe/Bucharest")
-        async def create_message(data):
+            await client.get_channel(813881450423386142).send(
+                embed=await self.create_embed_info("👉🏻 Statusul Aplicatiilor in Factiuni 👈🏻", 
+                                            "https://www.rpg2.b-zone.ro/factions/index",
+                                                data))
+            print("done scraping the factions")
+            await asyncio.sleep(10800)
+            
 
-            embed = discord.Embed(title="👉🏻 Situatia turfurilor in mafii 👈🏻", 
-                                url="https://www.rpg2.b-zone.ro/wars/turfs",
-                                description=f"⏰ Ultima data actualizat la {datetime.now(tz)}",
-                                color=0x00ff62)
-            embed.set_author(name="Danix43", icon_url="https://cdn.discordapp.com/avatars/783680772014997546/92a2a4d507b520d27aa91121a8dece50.png")
-            embed.set_thumbnail(url="http://i.imgur.com/Z3UHdYS.png")
-            for k, v in data.items():
-                embed.add_field(name=k, value=v, inline=False)
-            return embed
+    async def scrap_turfs(self):
         await self.wait_until_ready()
         data = await self.scraper.check_turfs()
         print("sending message")
         # dev channel
-        # await client.get_channel(813834934673735761).send(embed=await create_message(data))
+        # await client.get_channel(813834934673735761).send(
+        #     embed=await self.create_embed_info("👉🏻Situatia turfurilor in mafii👈🏻", 
+        #                                     "https://www.rpg2.b-zone.ro/wars/turfs",
+        #                                     data))
         # prod channel
-        await client.get_channel(813881497525289031).send(embed=await create_message(data))
-        await asyncio.sleep(86400)
-        print("done, waiting")
-        print("done waiting, resuming")
+        await client.get_channel(813881497525289031).send(
+            embed=await self.create_embed_info("👉🏻Situatia turfurilor in mafii👈🏻", 
+                                            "https://www.rpg2.b-zone.ro/wars/turfs",
+                                            data))
+        print("done scraping the turfs")
 
 
 intents = discord.Intents.default()
